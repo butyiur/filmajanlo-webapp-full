d88ed8501d4ffefb5db4a1df1cef281ff857690b
Molnár Attila Mátyás <molnarattilamatyas@mailbox.unideb.hu>
2025-11-27 11:31:05 +0100

    Initialize Spring Boot backend project (structure + Maven setup)

---
+++ backend/src/main/java/hu/attila/filmajanlo/FilmAjanloBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.attila.filmajanlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmAjanloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmAjanloBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hu/attila/filmajanlo/FilmAjanloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.attila.filmajanlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmAjanloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
